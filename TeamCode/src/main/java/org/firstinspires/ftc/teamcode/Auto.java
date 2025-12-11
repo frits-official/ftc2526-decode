b5d3dd9e83c9a1290ef9766ee8832cc91c09a1ca
bachvdfhl32851-ui <bachvdfhl32851@gmail.com>
2025-12-11 20:59:02 +0700

    test auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -2,25 +2,40 @@ package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower;
 
+import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous
-public class Auto_test extends OpMode {
+public class Auto extends OpMode {
     private Follower follower = null;
     private TelemetryManager telemetryM = null;
+    private final Pose scorePose = new Pose(0, 30, Math.toRadians(90));
+    private final Pose pickup1Pose = new Pose(30, 0, 0);
 
     @Override
     public void init() {
         follower = createFollower(hardwareMap);
         follower.setPose(new Pose(0,0,0));
+
+        PathChain pathChain = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, pickup1Pose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
+                .addPath(new BezierLine(pickup1Pose, scorePose))
+                .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        follower.followPath(pathChain);
+
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
     }
 
     public void loop() {
-        
 
         follower.update();
         telemetryM.debug("X:" + follower.getPose().getX());
