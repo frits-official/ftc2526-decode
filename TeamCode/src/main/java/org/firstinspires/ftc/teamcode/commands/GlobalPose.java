b7ff3d495684858d32c9136938685164449b9e8b
bachvdfhl32851-ui <bachvdfhl32851@gmail.com>
2026-01-28 17:49:46 +0700

    update auto red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GlobalPose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GlobalPose.java
@@ -5,26 +5,26 @@ import com.pedropathing.geometry.Pose;
 public class GlobalPose {
     public static Pose lastPose;
 
-    public static class BLUE {
+    //public static class BLUE {
         public static final Pose pushLever = new Pose(10.12, 70.49, Math.toRadians(270));
         public static final Pose scorePoseEnd = new Pose(51.79, 106.09, Math.toRadians(135));
 
-        public static class BlueNearZonePose {
+       // public static class BlueNearZonePose {
             //Pose start, score
             public static final Pose startPose = new Pose(49.64, 137.14, Math.toRadians(180));
             public static final Pose scorePose = new Pose(48.43, 96.54, Math.toRadians(135));
             //Pose End
             public static final Pose endPose = new Pose(43.61, 72.41, Math.toRadians(180));
 
-        }
+        ///}
 
-        public static class BlueFarZonePose {
+       // public static class BlueFarZonePose {
             public static final Pose startPose = new Pose(47.56, 7.43, Math.toRadians(180));
             public static final Pose scorePose = new Pose(59.16, 21.1, Math.toRadians(111));
             public static final Pose endPose = new Pose(54.97, 37.15, Math.toRadians(111));
-        }
+        //}
 
-        public static class PICKUP_POSE_BLUE {
+        //public static class PICKUP_POSE_BLUE {
             public static final Pose pickup1_1 = new Pose(43.08, 83.27, Math.toRadians(180));
             public static final Pose pickup1_2 = new Pose(20.24, 83.27, Math.toRadians(180));
             public static final Pose pickup2_1 = new Pose(42.37, 59.32, Math.toRadians(180));
@@ -35,16 +35,38 @@ public class GlobalPose {
             public static final Pose pickup4_2 = new Pose(7.23, 10.08, Math.toRadians(270));
             public static final Pose indulge1 = new Pose(7.33, 25.67, Math.toRadians(90));
             public static final Pose indulge2 = new Pose(7.63, 54.51, Math.toRadians(90));
-        }
-    }
+       // }
+    //}
+
     public static class RED {
+        public static final Pose pushLever = BLUE.pushLever.mirror();
+        public static final Pose scorePoseEnd = BLUE.scorePoseEnd.mirror();
+
         public static class RedNearZonePose {
             //Pose start, score
             public static final Pose startPose = BLUE.BlueNearZonePose.startPose.mirror();
             public static final Pose scorePose = BLUE.BlueNearZonePose.scorePose.mirror();
             //Pose End
             public static final Pose endPose = BLUE.BlueNearZonePose.endPose.mirror();
+        }
 
+        public static class RedFarZonePose {
+            public static final Pose startPose = BLUE.BlueFarZonePose.startPose.mirror();
+            public static final Pose scorePose = BLUE.BlueFarZonePose.scorePose.mirror();
+            public static final Pose endPose = BLUE.BlueFarZonePose.endPose.mirror();
+        }
+
+        public static class PICKUP_POSE_RED {
+            public static final Pose pickup1_1 = BLUE.PICKUP_POSE_BLUE.pickup1_1.mirror();
+            public static final Pose pickup1_2 = BLUE.PICKUP_POSE_BLUE.pickup1_2.mirror();
+            public static final Pose pickup2_1 = BLUE.PICKUP_POSE_BLUE.pickup2_1.mirror();
+            public static final Pose pickup2_2 = BLUE.PICKUP_POSE_BLUE.pickup2_2.mirror();
+            public static final Pose pickup3_1 = BLUE.PICKUP_POSE_BLUE.pickup3_1.mirror();
+            public static final Pose pickup3_2 = BLUE.PICKUP_POSE_BLUE.pickup3_2.mirror();
+            public static final Pose pickup4_1 = BLUE.PICKUP_POSE_BLUE.pickup4_1.mirror();
+            public static final Pose pickup4_2 = BLUE.PICKUP_POSE_BLUE.pickup4_2.mirror();
+            public static final Pose indulge1 = BLUE.PICKUP_POSE_BLUE.indulge1.mirror();
+            public static final Pose indulge2 = BLUE.PICKUP_POSE_BLUE.indulge2.mirror();
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedAuto/NearZone/RedNearZonePath1_2_3Ball12NoLeverNoIndulge.java
@@ -0,0 +1,173 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto.RedAuto.NearZone;
+
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.commands.GlobalPose;
+
+@Autonomous
+public class RedNearZonePath1_2_3Ball12NoLeverNoIndulge extends LinearOpMode {
+    Robot robot = new Robot();
+    private int pathState;
+    private Timer pathTimer, opmodeTimer;
+    private ElapsedTime time = new ElapsedTime();
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            //Start
+            case 0:
+                robot.follower.followPath(robot.follower.pathBuilder()
+                        .addPath(new BezierLine(GlobalPose.RED.RedNearZonePose.startPose, GlobalPose.RED.RedNearZonePose.scorePose))
+                        .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                        .build());
+                setPathState(1);
+                break;
+            case 1:
+                if (!robot.follower.isBusy()) {
+                    robot.unBlockAndShoot();
+                    setPathState(2);
+                }
+                break;
+
+            //Path1
+            case 2:
+                if (!robot.running) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.PICKUP_POSE_RED.pickup1_1))
+                            .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.PICKUP_POSE_RED.pickup1_2))
+                            .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
+                            .build(), true);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+                if (!robot.follower.isBusy()) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.RedNearZonePose.scorePose))
+                            .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                            .build(), true);
+                    setPathState(4);
+                }
+                break;
+            case 4:
+                if (!robot.follower.isBusy()) {
+                    robot.unBlockAndShoot();
+                    setPathState(5);
+                }
+                break;
+
+            //Path2
+            case 5:
+                if (!robot.running) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.PICKUP_POSE_RED.pickup2_1))
+                            .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.PICKUP_POSE_RED.pickup2_2))
+                            .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
+                            .build(), true);
+                    setPathState(6);
+                }
+                break;
+            case 6:
+                if (!robot.follower.isBusy()) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.RedNearZonePose.scorePose))
+                            .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                            .build(), true);
+                    setPathState(7);
+                }
+                break;
+            case 7:
+                if (!robot.follower.isBusy()) {
+                    robot.unBlockAndShoot();
+                    setPathState(8);
+                }
+                break;
+
+            //Path3
+            case 8:
+                if (!robot.running) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.PICKUP_POSE_RED.pickup3_1))
+                            .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.PICKUP_POSE_RED.pickup3_2))
+                            .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
+                            .build(), true);
+                    setPathState(9);
+                }
+                break;
+            case 9:
+                if (!robot.follower.isBusy()) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.RedNearZonePose.scorePose))
+                            .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                            .build(), true);
+                    setPathState(10);
+                }
+                break;
+            case 10:
+                if (!robot.follower.isBusy()) {
+                    robot.unBlockAndShoot();
+                    setPathState(11);
+                }
+                break;
+
+            //End
+            case 11:
+                if (!robot.running) {
+                    robot.follower.followPath(robot.follower.pathBuilder()
+                            .addPath(new BezierLine(robot.follower.getPose(), GlobalPose.RED.RedNearZonePose.endPose))
+                            .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                            .build(), true);
+                    setPathState(12);
+                }
+                break;
+            case 12:
+                if (!robot.follower.isBusy()) {
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void runOpMode() {
+        robot.init(this, Constants.ALLIANCE.BLUE);
+        robot.setPose(GlobalPose.RED.RedNearZonePose.startPose);
+        robot.aimShoot(false, false);
+
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+        setPathState(0);
+
+        robot.turret.resetEncoder();
+
+        while (!isStarted()) {
+            robot.init_loop();
+        }
+
+        if (opModeIsActive()) {
+
+            while (opModeIsActive()) {
+                robot.update();
+                robot.aimShoot(true, true);
+                robot.intakeAuto(true);
+                autonomousPathUpdate();
+
+                robot.updateTelemetry(true, true, true, true);
+            }
+        }
+    }
+}
